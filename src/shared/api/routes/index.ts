3af9708c656a9aa2ff2593980bd36fe416abea69
Bueno Nepomuceno <buenonp@gmail.com>
2023-07-08 16:54:18 -0300

    adicionado o middleware autenticação nas rotas

--- src/shared/api/routes/index.ts
+++ src/shared/api/routes/index.ts
@@ -2,6 +2,8 @@ import { Router } from 'express';
 
 import authRouter from '@/modules/auth/auth/api/routes/auth.routes';
 
+import { ensureIsAuthenticated } from '../middlewares/AuthMiddleware';
+
 import crud1Router from '@/modules/cruds-example/crud-example1/api/routes/crud1.routes';
 import crud2Router from '@/modules/cruds-example/crud-example2/api/routes/crud2.routes';
 
@@ -9,6 +11,7 @@ const router = Router();
 
 router.use('/auth', authRouter);
 
+router.use(ensureIsAuthenticated);
 router.use('/crud-example1', crud1Router);
 router.use('/crud-example2', crud2Router);
 
