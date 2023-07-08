836637b93442cb8e165aa5b78a1f5e8b7cd65891
Bueno Nepomuceno <buenonp@gmail.com>
2023-07-08 16:48:35 -0300

    função getitembyfield adicionada no base

--- src/modules/base/base.routes.ts
+++ src/modules/base/base.routes.ts
@@ -5,6 +5,7 @@ const baseRoutes = (controller: any) => {
 
     router.get('/', (req, res) => controller.getItems(req, res));
     router.get('/:id', (req, res) => controller.getItemById(req, res));
+    router.get('/:field/:value', (req, res) => controller.getItemByField(req, res));
     router.post('/', (req, res) => controller.createItem(req, res));
     router.put('/:id', (req, res) => controller.updateItem(req, res));
     router.delete('/:id', (req, res) => controller.deleteItem(req, res));
