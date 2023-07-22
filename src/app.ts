4096bfd8779e85f5aa898c9b5396b322efdb3820
Bueno Nepomuceno <buenonp@gmail.com>
2023-07-22 10:16:31 -0300

    added cors

--- src/app.ts
+++ src/app.ts
@@ -6,10 +6,13 @@ import "./config/database/typeorm";
 import "./config/container/tsyringe";
 
 import express from "express";
+import cors from "cors";
 
 import router from "@/shared/api/routes";
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 
